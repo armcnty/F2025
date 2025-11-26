270b958fc572f0b7ddfa974497f00bfcb40eebf2
armcnty <62534101+armcnty@users.noreply.github.com>
2025-11-26 09:07:58 -0400

    Update index.js

--- VueJS-Examples/Axios-only/index.js
+++ VueJS-Examples/Axios-only/index.js
@@ -1,7 +1,7 @@
 const axiosRequest = require("axios");
 const apiUrl = "http://localhost:3000/contact/";
 // If not using your own API, try it out with this: 
-// https://www.boredapi.com/api/activity
+// https://api.github.com
 
 async function getActivity() {
     let response = await axiosRequest.get(apiUrl);
